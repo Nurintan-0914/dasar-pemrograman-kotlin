0ebe3e5cb55730321aa46a3fe72a8c2b8d3d2fb1
Nurintan <nurintann0914@gmail.com>
2024-09-02 22:33:27 +0700

    Add BlokdanKomentar.kt

---
+++ src/main/kotlin/BlokdanKomentar.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>){
+    println("Hello, ini program pertama saya di kotlin!")
+    //Contoh komentar satu baris
+    /*
+    Contoh komentar multi baris
+     */
+
+    /**
+     * kotlin Doc --> baris ni untuk mendokumentasikan kode program
+     */
+}

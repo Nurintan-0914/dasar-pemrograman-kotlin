174d7360e1cb303236968f2bb6ee460c4ad507b6
Nurintan <nurintann0914@gmail.com>
2024-09-02 23:14:26 +0700

    Add ConstantsLiteral.kt

---
+++ src/main/kotlin/ConstantsLiteral.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    val floatLiberal : Float = 178.95F //Literal Float
+    println ("Contoh Liberal Float : " + floatLiberal)
+    val oneBillion: Long = 100000000L //Literal long
+    println("Contoh Literal Long : " + oneBillion)
+    val valHeksa = 0x0F //Literal Heksadesimal diawali dengan 0x
+    println("Contoh Literal Heksadesimal : " + valHeksa)
+    val valBinary = 0b00001011 //literal binary diawali dengan 0b
+    println("Contoh Literal Binary : " +valBinary)
+    val notasiKonvensional = 123.5e10
+    println("Contoh Literasi Konvensional Floating Point Number : " +notasiKonvensional)
+}

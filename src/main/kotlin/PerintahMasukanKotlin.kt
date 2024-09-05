1ce4fa7de358aaa7d7acf117157ba4322a26c735
Nurintan <nurintann0914@gmail.com>
2024-09-05 10:14:44 +0700

    Add PerintahMasukanKotlin.kt

---
+++ src/main/kotlin/PerintahMasukanKotlin.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>){
+    //parrot program
+    println("hi i'm lora, the virtual parrot, and i love to repeat!");
+    println("type something in: ");
+    var input: String
+    input = readLine()!!
+    var output: String
+    output = input + "bla, " +input + "bla!"
+    println(output)
+
+}
